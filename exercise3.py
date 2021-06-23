bd545e04aeb00349e79894dd70c9cc7dc0ea6e03
Paul3111 <82970627+Paul3111@users.noreply.github.com>
2021-06-23 19:43:16 +0100

    partially done

--- exercise3.py
+++ exercise3.py
@@ -1,14 +1,30 @@
+import itertools
+
+combinations_list = []
+values_list = []
+z = []
+
 initial_dictionary = {"1": "abc", "2": "s", "3": "o", "4": "n", "5": "lm", "6": "jk", "7": "gi", "8": "def", "9": "abc"}
 
+for keys, values in initial_dictionary.items():
+    values_list.append(values)
 
-def combinations(dictionary: dict) -> str:
-    pass
-
-# map(function, data)
-# zip()
-# lambda()
+print(list(itertools.product(*values_list)))
 
 
-for key in initial_dictionary:
-    print(key)
+# z = []
+# a = "abc"
+# b = "c"
+# for letter in a:
+#     for letter2 in b:
+#         x = zip(letter, letter2)
+#         print(list(x))
+#         z.append(list(zip(letter, letter2)))
+#         print(z)
 
+# print(z)
+
+for string in initial_dictionary.values():
+    combinations_list.append(string)
+
+print(combinations_list)
